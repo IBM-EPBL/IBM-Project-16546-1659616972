7f3087c870e4371111114e488370b7f732e209a3
Ferdinacharles <113236011+Ferdinacharles@users.noreply.github.com>
2022-09-24 22:13:02 +0530

    Update PYTHON CODE.py

--- ASSIGNMENT 2/FERDINA CHARLES/PYTHON CODE.py
+++ ASSIGNMENT 2/FERDINA CHARLES/PYTHON CODE.py
@@ -5,7 +5,7 @@ while(1):
     humidity = random.randint(1,100)
     print("\nTemperature : {0} \nHumidity : {1}".format(temp,humidity))
     if(temp>50 and humidity < 50): 
-        print("Alarm Detected");
+        print("Alarm Detected")
     else:
         print("Alarm not detected")
     sleep(3)
